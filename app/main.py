3dab4fb94b4e6e275509c0d8384eeb5f6c704ed5
haruman <godjaewook@naver.com>
2024-01-18 12:15:44 +0000

    동의

---
+++ app/main.py
@@ -0,0 +1,51 @@
+import uvicorn
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+class HelloResponse(BaseModel):
+    version: str
+    template: dict
+
+def create_hello_response(template: dict) -> HelloResponse:
+    return HelloResponse(version="2.0", template=template)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.post("/api/sayHello")
+def say_hello():
+    template = {
+        "outputs": [
+            {
+                "simpleText": {
+                    "text": "AI"
+                }
+            }
+        ]
+    }
+    return create_hello_response(template)
+
+
+@app.post("/api/showHello")
+def show_hello():
+    template = {
+        "outputs": [
+            {
+                "simpleImage": {
+                    "imageUrl": "https://t1.daumcdn.net/friends/prod/category/M001_friends_ryan2.jpg",
+                    "altText": "hello I'm Ryan"
+                }
+            }
+        ]
+    }
+    return create_hello_response(template)
